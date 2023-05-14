9f75218cf59d1123e20388cfd141eaf23d59e16d
HassanBahati <mukisabahati@gmail.com>
2023-05-14 19:17:19 +0300

    chore: add middleware

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send(`hello, world`)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send(`hello, world`);
 });
 
 app.listen(port, () => {
